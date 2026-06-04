398dae934310a33af4b2e50a8e51310d8c936890
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-04 17:37:18 +0000

    conversion of datatypes

---
+++ basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+let score = "333"
+
+console.log(typeof score);
+
+let newValue = Number(score)
+console.log(typeof newValue);
+console.log(newValue);
+
+// "33" > 33
+// "33abc" > nan
+// true > 1
+//false > 0
+// null > 0
+
+let isLoggedIn = 1
+let newVa = Boolean(isLoggedIn)
+console.log(newVa);
+
+// 1 > true
+// 0 > false
+// " " > false
+// "vivek" > true
+// null > false
+// undefined > false
+
+let newN = 33
+let newNa = String(newN)
+console.log(newNa);
+console.log(typeof newNa);
+
+
