ae7984e5794b58875ff36a943a43548b1d217a6d
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-04 18:35:14 +0000

    operations explained

--- basics/03_conversionOperations.js
+++ basics/03_conversionOperations.js
@@ -1,10 +1,10 @@
 let score = "333"
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let newValue = Number(score)
-console.log(typeof newValue);
-console.log(newValue);
+//console.log(typeof newValue);
+//console.log(newValue);
 
 // "33" > 33
 // "33abc" > nan
@@ -14,7 +14,7 @@ console.log(newValue);
 
 let isLoggedIn = 1
 let newVa = Boolean(isLoggedIn)
-console.log(newVa);
+//console.log(newVa);
 
 // 1 > true
 // 0 > false
@@ -25,7 +25,59 @@ console.log(newVa);
 
 let newN = 33
 let newNa = String(newN)
-console.log(newNa);
-console.log(typeof newNa);
+//console.log(newNa);
+//console.log(typeof newNa);
+
+
+// **************** operations ***************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+// let str1 = "hello"
+// let str2 = " vivek"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// // num1 = num2 = num3 = 2+2
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+
+// postfix
+// let m1 = 4
+// let m2 = m1++
+// console.log(m1, m2)
+
+// prefix
+let m1 = 4
+let m2 = ++m1
+console.log(m1, m2)
+
+
+
+
+
+
+
+
 
 
