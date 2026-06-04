dece972c074acbc5127cd117b656e8d13e4509ba
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-04 17:11:41 +0000

    data types

---
+++ basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+ // alert(3 + 3) // we are using node.js not browser
+
+ console.log(3 + 3) // code readability should be high
+
+ console.log("vivek")
+
+ let name = "hitesh" // string
+ let age  = 18 // number
+ let isLoggedIn = false // boolean
+
+ // number > 2 ^ 53 sudhi
+ // bigInt > mota data mate
+ // string >" " (doubble quote ma use karvu)
+ // boolean > true/false
+ // null > special type (khali che em describe karay) not undefine
+ // undefine > value define na hoy
+ // symbol > uniqueness 
+
+ // object
+
+
+ console.log(typeof age);
+ console.log(typeof null); // output object aavse
+ console.log(typeof undefined); // output undefined aavse
+ 
