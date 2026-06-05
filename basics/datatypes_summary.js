d65a1169cbf92f52d14f6e67ef80cecf3098035c
vivekhadiya168 <vivekhadiya168@gmail.com>
2026-06-05 13:53:44 +0000

    datatypes summary

---
+++ basics/datatypes_summary.js
@@ -0,0 +1,42 @@
+// Primitive 
+
+// 7 types : string, number, boolean, null, undefined, symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false   // boolean
+// const Temp = null          // null (empty)
+// let userEmail;             // undefined
+
+// const bigNumber = 1234567n    // BigInt
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123') // symbol ni andar same value hoy pan unique name hoy to chale ex : id and anotherId
+
+// console.log(id);
+// console.log(id === anotherId); // both are not same
+
+
+
+// reference(non-primitive)
+
+// Arrays, Objects, Functions
+
+// const heros = ["shaktiman", "nagraj", "doga"]     // arrays
+
+// {
+//     name: "hitesh",
+//     age: 22,                // Objects. >> {} bracket ma lakhay
+// }
+
+
+// function(){}           // function ne aa rite lakay 
+
+// const myFunction = function(){
+//     console.log("hello world");
+    
+// }                                          // variable ma store karyu
+
+// non-primitive no data type object j aave.
+
